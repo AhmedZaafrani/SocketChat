6f535aa9b5ded3cf4cfe10b9d0589ba280460449
Kirito-C <149937627+Simone-Rossi-GB@users.noreply.github.com>
2025-04-13 04:22:06 +0200

    modificati parametri di alcune funzioni + eliminata la lista nomi poiché inutile

--- server.py
+++ server.py
@@ -3,24 +3,22 @@ import socket
 
 server_socket = None
 clients = []
-nomi = {}
 lock = threading.Lock
 
-def ascolta_client(client):
+def ascolta_client(client, address):
     client.send("Inserisci il tuo nome: ".encode('utf-8'))
     nome = client.recv(1024).decode('utf-8').strip()
 
     with lock:
         clients.append(client)
-        nomi[client] = nome
         messaggio_broadcast(f"{nome} si è unito alla chat".encode('utf-8'), None)
-        print(f"{nome} si è unito alla chat")
+        print(f"{nome} - {address} - si è unito alla chat") # log
 
     while True:
         try:
             message = client.recv(1024).decode('utf-8')
-            messaggio_broadcast(message, client)
-            print(f"{nomi[client]}: {message}")
+            messaggio_broadcast(f"{nome}: {message}", client)
+            print(f"{nome}: {message}")
 
         except Exception as e:
             print(f"eccezione nella gestione del client {client}. Info aggiuntive: {e}")
@@ -28,7 +26,8 @@ def ascolta_client(client):
     with lock:
         clients.remove(client)
         del nomi[client]
-        messaggio_broadcast(f"{nome} ha lasciato la chat", client, address)
+        messaggio_broadcast(f"{nome} ha lasciato la chat", client)
+        print(f"{nome} - {address} - ha lasciato la chat") # log
 
 
 def listen_for_client():
@@ -36,12 +35,12 @@ def listen_for_client():
     while True:
         try:
             client, address = server_socket.accept()
-            print(f"si è connesso al server: {address}")
-            client_thread = threading.Thread(ascolta_client, args=(client))
+            print(f"si è connesso al server: {address}") # log
+            client_thread = threading.Thread(ascolta_client, args=(client, address))
             client_thread.daemon = True
             client_thread.start()
         except Exception as e:
-            print(f"eccezzione nella funzione listen_for_client: {e}")
+            print(f"eccezione nella funzione listen_for_client: {e}")
             break
 
 def start_server():
