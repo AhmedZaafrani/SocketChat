9cbc4b9b8d4621084a0d65a7e76285de0e5b3a56
Kirito-C <149937627+Simone-Rossi-GB@users.noreply.github.com>
2025-04-13 04:33:14 +0200

    terminato il lavoro principale del server.py
    si vuole aggiungere un ciclo che tenga in esecuzione il server e che termini con un Keyboard Interrupt

--- server.py
+++ server.py
@@ -17,17 +17,20 @@ def ascolta_client(client, address):
     while True:
         try:
             message = client.recv(1024).decode('utf-8')
-            messaggio_broadcast(f"{nome}: {message}", client)
-            print(f"{nome}: {message}")
+            if message:
+                if message == "closed connection":
+                    break
+                messaggio_broadcast(f"{nome}: {message}", client)
+                print(f"{nome}: {message}")
 
         except Exception as e:
             print(f"eccezione nella gestione del client {client}. Info aggiuntive: {e}")
 
     with lock:
         clients.remove(client)
-        del nomi[client]
         messaggio_broadcast(f"{nome} ha lasciato la chat", client)
         print(f"{nome} - {address} - ha lasciato la chat") # log
+        client.close()
 
 
 def listen_for_client():
@@ -36,7 +39,7 @@ def listen_for_client():
         try:
             client, address = server_socket.accept()
             print(f"si è connesso al server: {address}") # log
-            client_thread = threading.Thread(ascolta_client, args=(client, address))
+            client_thread = threading.Thread(target=ascolta_client, args=(client, address))
             client_thread.daemon = True
             client_thread.start()
         except Exception as e:
@@ -47,17 +50,26 @@ def start_server():
     global server_socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind("127.0.0.1", 12345)
+    server_socket.bind(("127.0.0.1", 12345))
     server_socket.listen(10)
 
-def messaggio_broadcast(messagge, sender_client):
+def messaggio_broadcast(message, sender_client):
     with lock:
         for client in clients:
             if client != sender_client:
-                client.send(messagge.encode('utf-8'))
+                try:
+                    client.send(message)
+                except Exception as e:
+                    print(f"errore nella funzione broadcast. Info: {e}")
+                    # Se c'è un errore, il thread del client è attivo stranamente ma il client probabilmente si è disconnesso
+                    client.close()
+                    if client in clients:
+                        clients.remove(client)
 
 
 if __name__ == "__main__":
     start_server()
-    listening_thread = threading.Thread(listen_for_client,)
+    listening_thread = threading.Thread(target=listen_for_client,)
+    listening_thread.daemon = True
+    listening_thread.start()
 
